6eb6a0d8117d1ea9fb21253854c6a7523d6af650
loukas <1216915972@qq.com>
2026-04-13 10:24:30 +0800

    完善逻辑

--- include/mylib.h
+++ include/mylib.h
@@ -1,3 +1,13 @@
 #include <iostream>
+#pragma once
 
+#ifdef _WIN32
+  #ifdef MYLIB_EXPORTS
+    #define MYLIB_API __declspec(dllexport)
+  #else
+    #define MYLIB_API __declspec(dllimport)
+  #endif
+#else
+  #define MYLIB_API
+#endif
 int add(int a, int b);
